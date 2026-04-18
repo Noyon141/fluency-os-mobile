8ceb633a1e4b1ae0c7e0f0cbc4aec948414bd240
Noyon141 <mdnoyon1590852@gmail.com>
2026-04-18 19:03:38 +0600

    feat: implement sign-out functionality with loading state and notifications

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,44 @@
+import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "expo-router";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Toast } from "toastify-react-native";
 
 const HomeTab = () => {
+  const [loading, setLoading] = React.useState(false);
+
+  const router = useRouter();
+  const onSubmit = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          setLoading(false);
+          Toast.success("Signed out successfully!");
+          router.push("/sign-in");
+        },
+        onError: () => {
+          setLoading(false);
+          Toast.error("Failed to sign out.");
+        },
+        onRequest: () => {
+          setLoading(true);
+        },
+      },
+    });
+  };
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 items-center justify-center p-5">
       <Text>Home</Text>
+      <Button
+        onPress={onSubmit}
+        disabled={loading}
+        className="w-full mt-5 rounded-full"
+        variant={"destructive"}
+      >
+        <Text>Sign out</Text>
+      </Button>
     </SafeAreaView>
   );
 };
