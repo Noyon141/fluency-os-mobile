33daec29f485e9f7f179c4793226444cd000045e
Noyon141 <mdnoyon1590852@gmail.com>
2026-04-19 17:33:05 +0600

    feat: integrate QueryClientProvider into RootLayout for improved data fetching

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,23 @@ import { ThemeProvider } from "@/components/theme/theme-provider";
 import "@/global.css";
 
 import { PortalHost } from "@rn-primitives/portal";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import ToastManager from "toastify-react-native";
 
 export default function RootLayout() {
+  const queryClient = new QueryClient();
+
   return (
     <>
       <ThemeProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(root)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        </Stack>
+        <QueryClientProvider client={queryClient}>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(root)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack>
+        </QueryClientProvider>
         <PortalHost />
         <ToastManager />
       </ThemeProvider>
