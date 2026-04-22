9723dc0384c305931bebf81030048fa44ec9dc1d
Noyon141 <mdnoyon1590852@gmail.com>
2026-04-22 11:32:47 +0600

    feat: implement authentication routing logic in RootLayout and manage session state

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,21 +1,13 @@
-import { authClient } from "@/lib/auth-client";
-import { Redirect, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
 
 const AuthLayout = () => {
-  const session = authClient.useSession();
-
-  if (session.data) {
-    return <Redirect href="/(root)/(tabs)/home" />;
-  } else {
-    return (
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="sign-in" />
-        <Stack.Screen name="sign-up" />
-        <Stack.Screen name="onboarding" />
-      </Stack>
-    );
-  }
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="sign-in" />
+      <Stack.Screen name="sign-up" />
+      <Stack.Screen name="onboarding" />
+    </Stack>
+  );
 };
-
 export default AuthLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,41 @@
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import "@/global.css";
+import { authClient } from "@/lib/auth-client";
 
 import { PortalHost } from "@rn-primitives/portal";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import ToastManager from "toastify-react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const queryClient = new QueryClient();
+
+  const router = useRouter();
+  const segments = useSegments();
+
+  const { data: session, isPending } = authClient.useSession();
+
+  useEffect(() => {
+    // 1. Do absolutely nothing if Better Auth is still checking
+    if (isPending) return;
+
+    // 2. Determine where the user is currently trying to go
+    const inRootGroup = segments[0] === "(root)";
+
+    // 3. Execute the strict routing logic
+    if (session && !inRootGroup) {
+      // User is authenticated but stuck outside the main app -> force them in
+      router.replace("/(root)/(tabs)/home");
+    } else if (!session && inRootGroup) {
+      // User is NOT authenticated but trying to access the app -> kick them to onboarding
+      router.replace("/(auth)/onboarding");
+    }
+    SplashScreen.hideAsync();
+  }, [session, isPending, segments]);
 
   return (
     <>
